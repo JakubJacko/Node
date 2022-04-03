5a0dd20b2676931771c998d739bc639c078cb17b
JakubJacko <jakub.jacko@op.pl>
2022-04-03 11:03:08 +0200

    tab

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const app = express()
 
 app.use(express.static('public'))
 
-app.get('/', function(req, res) {
-    // res.send('Hello World!')
+
+app.get(['/kolko', '/krzyzyk'], function(req, res) {
+    console.log(__dirname)
+    res.sendFile(__dirname+'/public/template.html')
+})
+
+app.get('/data', (req, res) => {
+    console.log(req.query)
+    res.send('')
 })
 
 app.listen(80)
