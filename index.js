b718dfc9db9f8a158a8af587fea8a9164c1f66a5
JakubJacko <jakub.jacko@op.pl>
2022-04-03 11:47:39 +0200

    interwał

--- index.js
+++ index.js
@@ -2,18 +2,36 @@ console.log("hello world")
 const express = require('express')
 const app = express()
 
+
+const game = {
+    plane : ['','','','','','','','',''],
+    makeSign(player, index){
+        this.plane[index] = player;
+        // console.log(this.plane)
+    }
+
+
+}
+
 app.use(express.static('public'))
 
-
 app.get(['/kolko', '/krzyzyk'], function(req, res) {
-    console.log(__dirname)
+    // console.log(__dirname)
     res.sendFile(__dirname+'/public/template.html')
 })
 
 app.get('/data', (req, res) => {
-    console.log(req.query)
-    res.send('')
+    // console.log(req.query)
+    game.makeSign(req.query.player, req.query.index)
+    res.send()
 })
+
+app.get('/sendData', (req, res) => {
+    res.send(game.plane)
+
+
+})
+
 
 app.listen(80)
 
