1741cf955fd9fbdbecdd98e40a2858dce75d2d8d
JakubJacko <jakub.jacko@op.pl>
2022-04-03 15:50:22 +0200

    end

--- index.js
+++ index.js
@@ -7,16 +7,40 @@ const game = {
     plane : ['','','','','','','','',''],
     possibilities : ["/kolko","/krzyzyk"],
     turn : 0,
-    
+    over : false,
     makeSign(player, index){
-        if (this.possibilities[this.turn] == player){
+        if (this.possibilities[this.turn] == player && this.plane[index] == ''){
             this.plane[index] = player;
             this.turn = this.turn == 0 ? 1 : 0;
+            if(this.checkWin()){
+                this.over = true;
+            }
             
+
         }
 
-    }
-
+    },
+    checkWin(){
+        for(let i = 0; i < 3; i++ ){
+            console.log(i)
+            if(this.plane[i*3] !=''
+            && this.plane[i*3] == this.plane[i*3+1]
+            && this.plane[i*3] == this.plane[i*3+2]){
+               return true;
+            }
+            if(this.plane[i] !=""
+            && this.plane[i] == this.plane[i+3] 
+            && this.plane[i] == this.plane[i+6]){
+                return true;
+            }
+            if(this.plane[i] !=""
+            && this.plane[i] == this.plane[i+4] 
+            && this.plane[i] == this.plane[i+8]){
+                return true;
+            }
+        }
+        return false;
+    },
 
 }
 
